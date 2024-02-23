477b8ddd50730d0e589c02adeec6289408fbdb9d
Serhii Nebolsin <SNebolsin_c@spherasolutions.com>
2024-02-23 23:11:44 +0200

    render and move figures

--- js/modules/Tetris.js
+++ js/modules/Tetris.js
@@ -4,6 +4,19 @@ export class Tetris {
         this.field = [];
         this.fieldsWidth = 10;
         this.fieldHeight = 20;
+        this.lFigure = [
+            [1,11,21,2]
+        ]
+        // this.zFigure = [
+        //     [1,2,11,10]
+        // ]
+        this.figures = [
+             [[1,11,21,2]],
+             [[1,2,11,10]],
+             [[1,11,21,31]],
+             [[1,2,11,12]]
+        ]
+        this.randomFigure = this.figures[Math.floor(Math.random()*4)]
     }
     generateField() {
         this.field = new Array(this.fieldHeight).fill(new Array(this.fieldsWidth).fill(0))
@@ -27,6 +40,8 @@ export class Tetris {
         for(let i = 0; i < this.fieldsWidth * this.fieldHeight; i++) {
             let cell = document.createElement('div');
             cell.classList.add('cell', "cell--0")
+            cell.setAttribute('id',`id${i}`);
+            cell.innerText = `${i}`
             document.querySelector('.field').appendChild(cell)
         }
     }
@@ -36,4 +51,29 @@ export class Tetris {
             cell.classList.add('cell--' + this.field[Math.floor(cellIndex / this.fieldsWidth)][cellIndex % this.fieldsWidth])
         })
     }
+    renderFigure(){
+        for(let i = 0; i < this.randomFigure[0].length; i++){
+            // console.log(this.randomFigure[0][i])
+            document.querySelector(`#id${this.randomFigure[0][i]}`).classList.add('cell--1')
+        }
+    }
+    unRenderFigure(){
+        for(let i = 0; i < this.randomFigure[0].length; i++){
+            // console.log(this.lFigure[0][i])
+            document.querySelector(`#id${this.randomFigure[0][i]}`).classList.remove('cell--1')
+        }
+    }
+    moveFigure(){
+        setInterval(() => {
+            this.unRenderFigure()
+            console.log(Math.max(...this.randomFigure[0]))
+            if(Math.max(...this.randomFigure[0]) < this.fieldHeight * this.fieldsWidth - 10) {
+                this.randomFigure[0] = this.randomFigure[0].map(v=> v+10)
+            } 
+            this.renderFigure()
+        }, 1000);
+        // console.log(this.figures[0])
+        // console.log(Math.floor(Math.random()*4))
+    }
+
 }
